a35ccd8c609738856bb77dc301aa1e4b1dfc1ba9
G_Freeman <geofreeman369@gmail.com>
2022-09-04 13:45:02 +0100

    first commit

---
+++ rest-apis/inventory-system/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Item struct {
+	UID   string  `json:"UID"`
+	Name  string  `josh:"Name"`
+	Desc  string  `json:"Desc"`
+	Price float64 `json:"Price"`
+}
+
+var inventory []Item
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	// formated print to a certain variable
+	fmt.Fprintf(w, "Endpoints called: homepage()")
+}
+
+func getInventory(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println("Function Called: getInventory()")
+	json.NewEncoder(w).Encode(inventory)
+}
+
+func createItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var item Item
+	// Gets data we sent in our request
+	_ = json.NewDecoder(r.Body).Decode(&item)
+	// Store that data into the inventory
+	inventory = append(inventory, item)
+	// return it back so the user an verify what we stored
+	json.NewEncoder(w).Encode(item)
+}
+
+func updateItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var item Item
+	_ = json.NewDecoder(r.Body).Decode(&item)
+
+	params := mux.Vars(r)
+
+	// Delete the item at UID
+	_deleteItemAtUid(params["uid"])
+	// Create it with new data
+	inventory = append(inventory, item)
+
+	json.NewEncoder(w).Encode(inventory)
+}
+
+func deleteItem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	_deleteItemAtUid(params["uid"])
+	json.NewEncoder(w).Encode(inventory)
+}
+
+func _deleteItemAtUid(uid string) {
+	for index, item := range inventory {
+		if item.UID == uid {
+			// Delete item from slice
+			inventory = append(inventory[:index], inventory[index+1:]...)
+			break
+		}
+	}
+}
+
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/inventory", getInventory).Methods("GET")
+	router.HandleFunc("/inventory", createItem).Methods("POST")
+	router.HandleFunc("/inventory/{uid}", deleteItem).Methods("DELETE")
+	router.HandleFunc("/inventory/{uid}", updateItem).Methods("PUT")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func main() {
+	inventory = append(inventory, Item{
+		UID:   "0",
+		Name:  "Cheese",
+		Desc:  "A fine block of cheese",
+		Price: 4.99,
+	})
+	inventory = append(inventory, Item{
+		UID:   "1",
+		Name:  "Milk",
+		Desc:  "A jug of milk",
+		Price: 3.25,
+	})
+
+	handleRequests()
+}
