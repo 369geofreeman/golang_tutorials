ccbea6818165ad56fafd690c6cd663a61d7471c5
G_Freeman <geofreeman369@gmail.com>
2022-09-11 13:08:31 +0100

    Structs, Methods & Interfaces | TDD

---
+++ gwt/struct/shapes_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func TestParimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.00
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		// Using tt.name ffrom the case to use it as the 't.Run' test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+	}
+}
